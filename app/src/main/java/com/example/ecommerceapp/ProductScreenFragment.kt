b548d4bb7e5d4cd6167ce32a7a89d29acf313319
neelSaarIT <neeldoshi147@gmail.com>
2023-12-09 02:36:40 +0530

    Fragment & Gradle

---
+++ app/src/main/java/com/example/ecommerceapp/ProductScreenFragment.kt
@@ -0,0 +1,31 @@
+package com.example.ecommerceapp
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class ProductScreenFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = ProductScreenFragment()
+    }
+
+    private lateinit var viewModel: ProductScreenViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_product_screen, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(ProductScreenViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/example/ecommerceapp/ProductScreenViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.ecommerceapp
+
+import androidx.lifecycle.ViewModel
+
+class ProductScreenViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
