eae520d48c1df44e29d980d19689f2289a5fce74
Siddhesh Jondhale <84581657+Siddheshjondhale@users.noreply.github.com>
2023-12-09 03:30:24 +0530

    adapter code and added into homefrgment

--- app/src/main/java/com/example/ecommerceapp/ProductScreenFragment.kt
+++ app/src/main/java/com/example/ecommerceapp/ProductScreenFragment.kt
@@ -6,8 +6,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.ecommerceapp.ViewPageAdapter.ImageList
+import com.example.ecommerceapp.ViewPageAdapter.ImageViewCarouselAdapter
 
 class ProductScreenFragment : Fragment() {
+
 
     companion object {
         fun newInstance() = ProductScreenFragment()
@@ -17,17 +20,26 @@ class ProductScreenFragment : Fragment() {
     private var _binding: ProductScreenFragment? = null
     private val binding get() = _binding!!
 
+
+    private var imageList= mutableListOf<String>()
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+        val imageList = mutableListOf<ImageList>()
+
+        imageList.add(ImageList(R.drawable.ic_launcher_background))
+        imageList.add(ImageList(com.google.android.material.R.drawable.ic_clear_black_24))
+        imageList.add(ImageList(com.google.android.material.R.drawable.ic_keyboard_black_24dp))
+        imageList.add(ImageList(R.drawable.ic_launcher_background))
+
+        binding.carouselViewPager.adapter = HomeScreenCarouselViewPagerAdapter(imageList,binding.carouselViewPager)
+
+        binding.carouselViewPager.adapter=ImageViewCarouselAdapter(imageList,binding.carouselViewPager)
+
         return inflater.inflate(R.layout.fragment_product_screen, container, false)
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(ProductScreenViewModel::class.java)
-        // TODO: Use the ViewModel
-    }
 
 }
--- app/src/main/java/com/example/ecommerceapp/ViewPageAdapter/ImageViewCarouselAdapter.kt
+++ app/src/main/java/com/example/ecommerceapp/ViewPageAdapter/ImageViewCarouselAdapter.kt
@@ -2,12 +2,17 @@ package com.example.ecommerceapp.ViewPageAdapter
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
 import androidx.viewpager2.widget.ViewPager2
+import com.example.ecommerceapp.R
 import com.example.ecommerceapp.databinding.ItemCarouselBinding
 
 class ImageViewCarouselAdapter(private val imageList: MutableList<ImageList>, private val viewPager2: ViewPager2): RecyclerView.Adapter<ImageViewCarouselAdapter.MyViewHolder>() {
-    class MyViewHolder(val binding: ItemCarouselBinding) : RecyclerView.ViewHolder(binding.root)
+    class MyViewHolder(val binding: ItemCarouselBinding) : RecyclerView.ViewHolder(binding.root){
+        val carouselViewPager = itemView.findViewById<ImageView>(R.id.carouselViewPager)
+
+    }
 
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
@@ -16,8 +21,11 @@ class ImageViewCarouselAdapter(private val imageList: MutableList<ImageList>, pr
     }
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+      holder.binding.imageView.setImageResource(imageList[position].imageResourceId)
+
 
         //holder.binding.carouselImageView.setImageResource(imageList[position].imageResourceId)
+
     }
 
     override fun getItemCount(): Int {
